710fbd58cb861f01706e80a404d4bc9d3fa3f56e
JackArt237483 <vip.artem4567@mail.ru>
2023-08-04 08:08:39 +0300

    split contact join array method

---
+++ 8kata/NumberObject/МethodLasrIndex/app.js
@@ -0,0 +1,22 @@
+const str = "Hellow World Artem"
+
+
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+console.log(str.indexOf("e"))
+console.log(str.lastIndexOf("e"))
+console.log(str.search("e"))
+
+
+/**/
+/*
+1)то есть такая штука
+console.log(str.indexOf("e"))
+console.log(str.search("e"))
+вот эти два метода ищят самый первый
+элемент в строке
+
+console.log(str.lastIndexOf("e"))
+вот этоо метод самый последний элемент
+в строке
+*/
---
+++ Method Array/Split Join Conact/app.js
@@ -0,0 +1,40 @@
+// методы строкового объекта--concat() split() и его хорошего друга join()
+// задасча у тебя есть строка и аггумент на которм тебе надо это все разбить
+
+// const newWords = str.split(" ")
+// console.log(newWords)
+// const getNewWords = str.split("").join(sim)
+// console.log(getNewWords)
+// const af = getNewWords.join("")
+// console.log(af)
+
+
+const str = "Hi my name is Artem"
+const sim = "/"
+
+
+function getNewStr(str,sim) {
+  const newWords = str.split(" ")
+
+  const getNewWords =  newWords.map(words => words.split("").join(sim))
+
+  const result = getNewWords.join(" ")
+
+  return result
+}
+
+console.log(getNewStr(str,sim))
+
+/*
+1)Есть функция с двумя аргументами
+str - это строка и sim это аргумент на который мы будем разбивать строку
+2) const newWords = str.split(" ")
+вот эта запсись у тебя разделяет строчку по пробелам
+3)const getNewWords =  newWords.map(words => words.split("").join(sim))
+В этой строке мы создаем новый массив на основе старого
+то есть мы разделяем по символам и добовляем символы по символам
+4) const result = getNewWords.join(" ")
+
+  return result
+  потом мы береми и соединяем новый массив в строчку
+*/
