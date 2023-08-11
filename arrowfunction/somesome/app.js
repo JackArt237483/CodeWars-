e4bc092954b7ef00b09a69acc5cba48f78c64263
JackArt237483 <vip.artem4567@mail.ru>
2023-08-11 10:42:37 +0300

    some description

---
+++ arrowfunction/somesome/app.js
@@ -0,0 +1,30 @@
+// варианты записи функций как можно их записатьь
+
+// function GetNumber(a,b) {
+//   return a + b
+// }
+
+// const GetNumber = function(a,b) {
+//   return a + b
+// }
+
+// const GetNumber = (a,b) => a + b
+//это просто варианты функций как их можно записать
+
+// const arr = [1]
+
+// const gete = (arr,val1,val2) => {
+//   arr.push(val1)
+//   arr.push(val2)
+//   return arr
+// }
+
+// console.log(gete([55],2,3))
+
+// function plus(a,b,c,d,e){
+//   return a+b+c+d+e;
+// }
+// var arg1=[1,2,3,4,5];
+// var arg2=[2,3];
+// console.log(plus(...arg1));
+// console.log(plus(1,...arg2,4,5));
